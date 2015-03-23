d6ac28ccf3670b199ec0807135f952e8c4ced9a0
woodsk9710 <woodsk9710@ATC104B08.ad.faytechcc.edu>
2015-03-23 12:22:59 -0400

    Messages display on separate lines.

--- src/hello/world/HelloWorld.java
+++ src/hello/world/HelloWorld.java
@@ -16,8 +16,8 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("Hello World");
-        System.out.print("Goodbye world");
+        System.out.println("Hello World");
+        System.out.println("Goodbye world");
     }
     
 }
